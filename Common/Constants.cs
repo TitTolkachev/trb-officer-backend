d5fdca7f40463b51b96b4013bf27a782d8c5686e
Tit Tolkachev <tittolkachev@gmail.com>
2024-03-10 14:38:23 +0700

    Updated Hosts.

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -6,7 +6,7 @@ public static class Constants
     public const string LoanHttpClient = "LOAN_HTTP_CLIENT";
     public const string UserHttpClient = "USER_HTTP_CLIENT";
 
-    public const string CoreHost = "http://188.235.125.159:8080/api/v1/";
-    public const string LoanHost = "http://188.235.125.159:8081/api/v1/";
-    public const string UserHost = "http://188.235.125.159:8082/api/v1/";
+    public const string CoreHost = "http://147.45.69.93:8080/api/v1/";
+    public const string LoanHost = "http://147.45.69.93:8081/api/v1/";
+    public const string UserHost = "http://147.45.69.93:8082/api/v1/";
 }
