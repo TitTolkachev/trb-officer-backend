18f5f7752ab5f3aecd62933fb5e136cafb755a35
Tit Tolkachev <tittolkachev@gmail.com>
2024-04-01 02:08:13 +0700

    Changed Server.

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -6,9 +6,13 @@ public static class Constants
     public const string LoanHttpClient = "LOAN_HTTP_CLIENT";
     public const string UserHttpClient = "USER_HTTP_CLIENT";
 
-    public const string CoreHost = "http://176.209.153.127:50052/api/v1/";
-    public const string LoanHost = "http://176.209.153.127:50053/api/v1/";
-    public const string UserHost = "http://176.209.153.127:50054/api/v1/";
+    // public const string CoreHost = "http://176.209.153.127:50052/api/v1/";
+    // public const string LoanHost = "http://176.209.153.127:50053/api/v1/";
+    // public const string UserHost = "http://176.209.153.127:50054/api/v1/";
+    // public const string KafkaHost = "176.209.153.127:50061";
 
-    public const string KafkaHost = "176.209.153.127:50061";
+    public const string CoreHost = "http://195.2.84.62:8080/api/v1/";
+    public const string LoanHost = "http://195.2.84.62:8081/api/v1/";
+    public const string UserHost = "http://195.2.84.62:8082/api/v1/";
+    public const string KafkaHost = "195.2.84.62:8100";
 }
