0c156eaac2c15ccc9715764dec386075fa01b04b
Tit Tolkachev <tittolkachev@gmail.com>
2024-05-20 09:48:13 +0700

    IP update.

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -6,18 +6,8 @@ public static class Constants
     public const string LoanHttpClient = "LOAN_HTTP_CLIENT";
     public const string UserHttpClient = "USER_HTTP_CLIENT";
 
-    // public const string CoreHost = "http://176.209.153.127:50052/api/v1/";
-    // public const string LoanHost = "http://176.209.153.127:50053/api/v1/";
-    // public const string UserHost = "http://176.209.153.127:50054/api/v1/";
-    // public const string KafkaHost = "176.209.153.127:50061";
-
-    public const string CoreHost = "http://5.42.105.18:8080/api/v1/";
-    public const string LoanHost = "http://5.42.105.18:8081/api/v1/";
-    public const string UserHost = "http://5.42.105.18:8082/api/v1/";
-    public const string KafkaHost = "5.42.105.18:8100";
-
-    // public const string CoreHost = "http://77.106.105.103:8080/api/v1/";
-    // public const string LoanHost = "http://77.106.105.103:8081/api/v1/";
-    // public const string UserHost = "http://77.106.105.103:8082/api/v1/";
-    // public const string KafkaHost = "195.2.84.62:8100";
+    public const string CoreHost = "http://81.19.137.198:8080/api/v1/";
+    public const string LoanHost = "http://81.19.137.198:8081/api/v1/";
+    public const string UserHost = "http://81.19.137.198:8082/api/v1/";
+    public const string KafkaHost = "81.19.137.198:8100";
 }
